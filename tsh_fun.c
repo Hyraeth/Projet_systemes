6f61e8ca44be0b3ae89724e9922300a50d4c2046
Lilian <lcambillau@orange.fr>
2020-10-21 23:40:01 +0200

    Fonction parsePathAbsolute finie

--- tsh_fun.c
+++ tsh_fun.c
@@ -1,9 +1,12 @@
+#define _GNU_SOURCE
+
 #include "tsh_fun.h"
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-char ***parsePathAbsolute (char *path) {
+char **parsePathAbsolute (char *path) {
     char *pathHere = get_current_dir_name();
     int size_1;
     char **pathHereArray = parse_path_array(pathHere,&size_1);
@@ -12,34 +15,50 @@ char ***parsePathAbsolute (char *path) {
     char **pathArray = parse_path_array(path,&size_2);
 
     int i = 0;
-    int nbElementsBefore_1 = size_1 - 1;
     int nbElementsBefore_2 = 0;
+
     while (pathArray[i] != NULL) {
         if (strcmp(pathArray[i],"..") == 0) {
             if (nbElementsBefore_2 == 0) {
-                if (nbElementsBefore_2 == 0) {
-                    perror("tsh");
+                if ((size_1 - 1) == 0) {
+                    perror("tsh no such file or directory parsePathAbsolute");
                 }
                 else {
-                    realloc(pathHereArray,nbElementsBefore_1);
-                    pathHereArray[nbElementsBefore_1 - 1] = NULL;
-                    nbElementsBefore_1 -- ;
+                    if ((pathHereArray = realloc(pathHereArray,(size_1 - 1) * sizeof(char *) )) == NULL){
+                    	perror ("tsh realloc parsePathAbsolute");
+                	}
+                	size_1--;
+                    pathHereArray[size_1 - 1] = NULL;
+
+                    memmove(pathArray, &pathArray[1], (size_2 - 1)*sizeof( char *) );
+
+	                if ((pathArray = realloc(pathArray,(size_2 - 1) * sizeof(char *))) == NULL) {
+	                	perror ("tsh realloc parsePathAbsolute");
+	                }
+	                size_2 --;
                 }
             }
             else {
                 memmove(&pathArray[nbElementsBefore_2 - 1],
                         &pathArray[nbElementsBefore_2],
-                        (size_1 - 1 - nbElementsBefore_2)*sizeof( char *) );
-                realloc(pathHereArray,size_1 - 1);
-                size_1 --;
+                        (size_2 - 1 - nbElementsBefore_2)*sizeof( char *) );
+                if ((pathArray = realloc(pathArray,(size_2 - 1) * sizeof(char *))) == NULL) {
+                	perror ("tsh realloc parsePathAbsolute");
+                }
+                size_2 --;
                 nbElementsBefore_2 --;
             }
         }
+        else {
+        	i++;
+        }
     }
-    char ***path_res = malloc( 2 * sizeof(char **));
-    path_res[0] = pathHereArray;
-    path_res[1] = pathArray;
-    return path_res
+    if ((pathHereArray = realloc(pathHereArray,(size_1 + size_2) * sizeof(char *) )) == NULL){
+    	perror ("tsh realloc parsePathAbsolute");
+	}
+	memcpy(&pathHereArray[size_1 - 1],pathArray, size_2 * sizeof( char * ));
+	free(pathArray);
+    return pathHereArray;
 }
 
 char **parse_path_array(char *path, int *a) {
@@ -52,7 +71,7 @@ char **parse_path_array(char *path, int *a) {
     while(word != NULL) {
 
         if (nbword == nbargs) {
-            nbargs += NBARGS;
+            nbargs += 8;
             if((args = realloc(args, nbargs*sizeof(char *))) == NULL)
                 perror("tsh realloc parse path");
         }
@@ -64,6 +83,22 @@ char **parse_path_array(char *path, int *a) {
         word = strtok(NULL, "/");
     }
     args[nbword] = NULL;
-    &a = nbword + 1;
+    *a = nbword + 1;
     return args;
+}
+
+void printArray (char **path1) {
+	int i = 0;
+	while (path1[i] != NULL) {
+		printf("/%s",path1[i]);
+		i++;
+	}
+	printf("\n");
+}
+
+int main(int argc, char const *argv[])
+{
+	char **res = parsePathAbsolute(argv[1]);
+	printArray(res);
+	return 0;
 }
--- tsh_fun.h
+++ tsh_fun.h
@@ -1,7 +1,8 @@
 #ifndef TSH_FUN_H
 #define TSH_FUN_H
 
-char ***parsePathAbsolute (char *path);
-char **parse_path_array(char *path);
+char **parsePathAbsolute (char *path);
+char **parse_path_array(char *path,int *a);
+void printArray (char **path1);
 
 #endif //TSH_FUN_H
