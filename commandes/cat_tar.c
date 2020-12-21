e2d35d67209d7ea7514b61b1fabb65dd1edbe00f
Yannis Didelot <yannis.didelot@free.fr>
2020-12-21 16:49:14 +0100

    la base du cat

--- commandes/cat_tar.c
+++ commandes/cat_tar.c
@@ -1,5 +1,10 @@
 #include "../headers/cat_tar.h"
 
+/**
+ *  @brief command cat on one tar files
+ *  @param fd the oppen tar file
+ *  @return 1 if the cat worked, and -1 if not
+*/
 int cat_tar(int fd) {
   struct posix_header * header = malloc(sizeof(struct posix_header));
   assert(header);
@@ -13,5 +18,19 @@ int cat_tar(int fd) {
   return 1;
 }
 
+/**
+ *  @brief 
+ *  
+*/
 int main(int argc, char *argv[]){
+  if(argc <= 2) printf("Pas de fichier\n");
+  else {
+    int fd = open(argv[1], O_RDONLY);
+    ls_tar('0', argv[2], fd);
+    close(fd);
+    write(STDOUT_FILENO, "\n", strlen("\n"));
+    fd = open(argv[1], O_RDONLY);
+    ls_tar('l', argv[2], fd);
+    close(fd);
+  }
 }
