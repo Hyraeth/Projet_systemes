e3840a29ff6f79eb3593a4616afd9e13de709a2f
Hyraeth <williamkhieu@gmail.com>
2020-12-28 12:46:05 +0100

    printmessage err

--- commandes/rmdir_tar.c
+++ commandes/rmdir_tar.c
@@ -1,36 +1,46 @@
 #include "../headers/rmdir_tar.h"
 
-int rmdirInTar (pathStruct *pathToDelete) {
-    char c = typeFile(pathToDelete->path,pathToDelete->nameInTar);
-	if (c == '9') {
-		printMessageTsh("Veuillez vérifier que le dossier que vous voulez supprimer existe bien");
-		return -1;
-	}
-
-    if (c == '5') {
-        return rmEmptyDirTar(pathToDelete->path,pathToDelete->nameInTar);
+int rmdirInTar(pathStruct *pathToDelete)
+{
+    char c = typeFile(pathToDelete->path, pathToDelete->nameInTar);
+    if (c == '9')
+    {
+        printMessageTsh(STDERR_FILENO, "Veuillez vérifier que le dossier que vous voulez supprimer existe bien");
+        return -1;
     }
-    else {
-        printMessageTsh("Pour supprimer autre chose qu'un dossier, veuillez utiliser la commande rm");
+
+    if (c == '5')
+    {
+        return rmEmptyDirTar(pathToDelete->path, pathToDelete->nameInTar);
+    }
+    else
+    {
+        printMessageTsh(STDERR_FILENO, "Pour supprimer autre chose qu'un dossier, veuillez utiliser la commande rm");
         return -1;
     }
 }
 
-int rmdirTar (char *path) {
-    if (!doesTarExist(path)) {
-        printMessageTsh("Le tar que vous voulez supprimer n'existe pas");
+int rmdirTar(char *path)
+{
+    if (!doesTarExist(path))
+    {
+        printMessageTsh(STDERR_FILENO, "Le tar que vous voulez supprimer n'existe pas");
         return -1;
     }
 
-    if (isEmptyTar(path)) {
-        if (remove(path) == 0) {
+    if (isEmptyTar(path))
+    {
+        if (remove(path) == 0)
+        {
             return 1;
-        } 
-        else {
+        }
+        else
+        {
             return -1;
         }
-	}
-	else {
-		printMessageTsh("Le tar que vous voulez supprimer n'est pas vide");
-	}
+    }
+    else
+    {
+        printMessageTsh(STDERR_FILENO, "Le tar que vous voulez supprimer n'est pas vide");
+    }
 }
